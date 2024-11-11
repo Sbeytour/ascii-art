514388b1689fe7c2eca2d292b3f922ed9e6592fb
sbeytour <beytour.safae@gmail.com>
2024-11-11 13:00:24 +0100

    program entry points and banners

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module ascii
+module reverse
 
 go 1.22.3
--- main.go
+++ main.go
@@ -1,1 +1,50 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"reverse/src"
+)
+
+func main() {
+	args := os.Args
+	bannerFile, input := rev.CheckArgs(args)
+	if rev.ReverseFile != "" {
+		asciiMap, err := rev.CreateMap("banners/standard.txt")
+		if err != nil {
+			log.Fatalln("Error of reading the file and crating Map")
+		}
+
+		inputFile, err := rev.ManageRevFile(rev.ReverseFile)
+		if err != nil {
+			log.Fatalln("Error of reading the input file")
+		}
+		// fmt.Println(inputFile)
+		// if inputFile[0] == "" {
+		// 	log.Fatalln("Error of the file")
+		// }
+		printing := rev.Reverse(inputFile, asciiMap)
+		fmt.Println(printing)
+	} else {
+		asciiArt := ""
+		asciiMap, err := rev.CreateMap(bannerFile)
+		if err != nil {
+			log.Fatalln("Error of reading the file")
+		}
+
+		if input == "" {
+			os.Remove(rev.OutputFile)
+			os.Exit(0)
+		}
+
+		splittedInput := rev.SplitInput(input)
+		asciiArt = rev.GenrateArt(asciiMap, splittedInput)
+		err = rev.CreateFile(asciiArt)
+		if err != nil {
+			log.Fatalln("error of creating file")
+		}
+
+	}
+}
